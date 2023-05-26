55d6eca3f12d4413a02b139cff76badccd03ffc9
성수천 <3598ysm@gmail.com>
2023-05-26 15:34:50 +0900

    BoardController 생성

--- boardver2/src/main/java/com/green/boardver2/ssc/BoardController.java
+++ boardver2/src/main/java/com/green/boardver2/ssc/BoardController.java
@@ -1,4 +1,4 @@
-package com.green.boardver2.ssc.board;
+package com.green.boardver2.ssc;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
