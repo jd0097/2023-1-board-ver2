b7022c87050935dd0bd44e1b83314256c4449751
jd0097 <131757796+jd0097@users.noreply.github.com>
2023-07-27 19:16:27 +0900

    Update Boardver2Application.java

--- boardver2/src/main/java/com/green/boardver2/Boardver2Application.java
+++ boardver2/src/main/java/com/green/boardver2/Boardver2Application.java
@@ -3,7 +3,7 @@ package com.green.boardver2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegisty;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
