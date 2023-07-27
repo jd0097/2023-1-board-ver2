2f7016338f8357b7f3f893e0923415ec54931fdf
jd0097 <131757796+jd0097@users.noreply.github.com>
2023-07-27 19:07:09 +0900

    Update Boardver2Application.java

--- boardver2/src/main/java/com/green/boardver2/Boardver2Application.java
+++ boardver2/src/main/java/com/green/boardver2/Boardver2Application.java
@@ -2,6 +2,9 @@ package com.green.boardver2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegisty;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Boardver2Application {
@@ -9,5 +12,20 @@ public class Boardver2Application {
     public static void main(String[] args) {
         SpringApplication.run(Boardver2Application.class, args);
     }
-
+@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
+						.allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST…)
+						.allowedOrigins("https://web-study-hub-rt92alkkyddso.sel4.cloudtype.app");
+			}
+		};
+	}
 }
+
+
+
+
